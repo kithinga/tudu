d01356f5d88d276082596b984a478de94011a0e0
Kithinga <menscfesz@gmail.com>
2022-03-05 20:10:46 +0300

    committed

--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -2,6 +2,12 @@
   <div class="container-fluid">
       <div class="row">
           <div class="col">
+              <div class="tsk01">
+                  <h4>Short me one</h4>
+                  <p>Fuck daddy and daughter onedks</p>
+              </div>
+          </div>
+          <div class="col">
               jifunze kunyamaza
           </div>
           <div class="col">
