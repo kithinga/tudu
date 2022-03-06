5997a6eb7b6cf183fe6e1fd9b2a9b4c73f6ffa79
Kithinga <menscfesz@gmail.com>
2022-03-06 07:22:41 +0300

    commits

---
+++ src/components/Navigator.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container-fluid">
+    <div class="row no-gutters">
+      <div class="col">
+        <div class="fute"><button class="nvbt">Futures</button></div>
+      </div>
+      <div class="col">
+        <div class="pend"><button class="nvbt">Pending</button></div>
+      </div>
+      <div class="col">
+        <div class="perf">
+            <div> <button class="nvbt">Perfomance</button></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<style>
+  
+</style>
+
