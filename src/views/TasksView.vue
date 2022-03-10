88dac419bacb0bcf7bc79fe2f8bc147b07b4fb21
Kithinga <menscfesz@gmail.com>
2022-03-10 20:31:29 +0300

    commit

--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -1,7 +1,50 @@
 
 <template>
   <Navigator />
-  <div><h4 class="tihead">Tasks</h4></div>
+
+  <div class="container-fluid">
+    <div class="row no-gutters checker">
+      <div class="col-lg-6">
+        <div class="tid-task"><h4 class="tihead">Tasks</h4></div>
+      </div>
+      <div class="col-lg-6">
+        <div class="tid-btn">
+          <!-- Button to trigger modal -->
+          <button
+            type="button"
+            class="btn-addtask shadow-sm"
+            data-bs-toggle="modal"
+            data-bs-target="#exampleModal"
+          >
+              <span class="material-icons">add</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Modal just laying around -->
+  <div
+    class="modal fade"
+    id="exampleModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+   >
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h6 class="modal-title" id="exampleModalLabel">  <span class="material-icons">task</span>Tasks Add tasks</h6>
+        </div>
+        <div class="modal-body">
+             <form action="">
+               <input type="text">
+             </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <div class="container-fluid">
     <div class="row no-gutters justify-content-center">
       <div class="col-lg-2" v-for="post of posts" :key="post.id">
@@ -18,6 +61,7 @@
 // @ serves as src alias
 import Navigator from "@/components/Navigator.vue";
 import axios from "axios";
+const baseURL = "http://localhost:3000/posts";
 export default {
   components: {
     Navigator,
@@ -25,17 +69,25 @@ export default {
   data() {
     return {
       posts: [],
+      taskName: "",
     };
   },
   async created() {
     try {
-      const res = await axios.get(`http://localhost:3000/posts`);
+      const res = await axios.get(baseURL);
       this.posts = res.data;
     } catch (e) {
       console.log(e);
     }
   },
+  methods: {
+    async addTask() {
+      const res = await axios.post(baseURL, { name: this.taskName });
+      this.posts = [...this.todo, { name: this.taskName }];
+    },
+  },
 };
+
 </script>
 
 
