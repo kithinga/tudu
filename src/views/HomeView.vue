636d81e661c65f241436037cb4d9b5d471d27cd6
Kithinga <menscfesz@gmail.com>
2022-03-27 22:18:13 +0300

    commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,21 +5,24 @@
         <div class="col-lg-2">
           <div class="titles">
             <p class="titl">TODOS FOR <span class="sel"> Better me</span></p>
-            <p class="ru">Consistency for - <span class="selm">Self Mastery . .</span></p>
+            <p class="ru">
+              Consistency for - <span class="selm">Self Mastery . .</span>
+            </p>
           </div>
         </div>
 
         <div class="col-lg-3">
           <div class="reminder">
+            
             <!-- <p class="rder">Overdue Tasks <span class="ress"> 47</span></p> -->
           </div>
         </div>
 
         <div class="col-lg-2">
           <div class="period">
-            <p class="time">00  10</p>
+            <p class="time">00 10</p>
             <p class="day">Sunday</p>
-            <p class="date">13   03   2022</p>
+            <p class="date">13 03 2022</p>
           </div>
         </div>
       </div>
@@ -28,86 +31,90 @@
     <!-- tudu cards for tas*                    -->
     <div class="container-fluid">
       <div class="row no-gutters card-row justify-content-center">
-        
-        <div class="col-lg-2">
-          <router-link to="/tasks">
-            <div class="card tasks shadow-sm">
-              <div class="card-body">
-                <h5 class="card-title">Tasks</h5>
-                <p class="material-icons md-32 icn">task</p>
-                <img
-                  src="../assets/images/Task.png"
-                  class="img-fluid"
-                  alt="..."
-                />
-              </div>
+   
+      
+            <div class="col-lg-2">
+              <router-link to="/tasks">
+                <div class="card tasks shadow-sm">
+                  <div class="card-body">
+                    <h5 class="card-title">Tasks</h5>
+                    <p class="material-icons md-32 icn">task</p>
+                    <img
+                      src="../assets/images/Task.png"
+                      class="img-fluid"
+                      alt="..."
+                    />
+                  </div>
+                </div>
+              </router-link>
             </div>
-          </router-link>
-        </div>
 
-        <div class="col-lg-2" >
-          <router-link to="/future">
-            <div class="card future shadow-sm">
-              <div class="card-body">
-                <h5 class="card-title">Futures</h5>
-                <p class="material-icons icn">next_plan</p>
-                <img
-                  src="../assets/images/Future.png"
-                  class="img-fluid"
-                  alt="..."
-                />
-              </div>
+            <div class="col-lg-2">
+              <router-link to="/future">
+                <div class="card future shadow-sm">
+                  <div class="card-body">
+                    <h5 class="card-title">Futures</h5>
+                    <p class="material-icons icn">next_plan</p>
+                    <img
+                      src="../assets/images/Future.png"
+                      class="img-fluid"
+                      alt="..."
+                    />
+                  </div>
+                </div>
+              </router-link>
             </div>
-          </router-link>
-        </div>
 
-        <div class="col-lg-2">
-          <router-link to="/pending">
-            <div class="card pending shadow-sm">
-              <div class="card-body">
-                <h5 class="card-title">Pending</h5>
-                <p class="material-icons icn">pending_actions</p>
-                <img
-                  src="../assets/images/Pending.png"
-                  class="img-fluid"
-                  alt=""
-                />
-              </div>
+            <div class="col-lg-2">
+              <router-link to="/pending">
+                <div class="card pending shadow-sm">
+                  <div class="card-body">
+                    <h5 class="card-title">Pending</h5>
+                    <p class="material-icons icn">pending_actions</p>
+                    <img
+                      src="../assets/images/Pending.png"
+                      class="img-fluid"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </router-link>
             </div>
-          </router-link>
-        </div>
 
-        <div class="col-lg-2">
-          <router-link to="/perfomance">
-            <div class="card perfomance shadow-sm">
-              <div class="card-body">
-                <h5 class="card-title">Perfomance</h5>
-                <p class="material-icons icn">timeline</p>
-                <img
-                  src="../assets/images/Perfomance01.png"
-                  class="img-fluid"
-                  alt=""
-                />
-              </div>
+            <div class="col-lg-2">
+              <router-link to="/perfomance">
+                <div class="card perfomance shadow-sm">
+                  <div class="card-body">
+                    <h5 class="card-title">Perfomance</h5>
+                    <p class="material-icons icn">timeline</p>
+                    <img
+                      src="../assets/images/Perfomance01.png"
+                      class="img-fluid"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </router-link>
             </div>
-          </router-link>
-        </div>
-        <!-- Event calendar -->
-        <div class="col-lg-2">
-          <router-link to="/perfomance">
-            <div class="card event shadow-sm">
-              <div class="card-body">
-                <h5 class="card-title">Event-Calendar</h5>
-                <p class="material-icons icn">event</p>
-                <img
-                  src="../assets/images/Perfomance01.png"
-                  class="img-fluid"
-                  alt=""
-                />
-              </div>
-            </div>
-          </router-link>
-        </div>
+            <!-- Event calendar -->
+            <!-- <div class="col-lg-2">
+              <router-link to="/perfomance">
+                <div class="card event shadow-sm">
+                  <div class="card-body">
+                    <h5 class="card-title">Event-Calendar</h5>
+                    <p class="material-icons icn">event</p>
+                    <img
+                      src="../assets/images/Perfomance01.png"
+                      class="img-fluid"
+                      alt=""
+                    />
+                  </div>
+                </div>
+              </router-link>
+            </div> -->
+
+
+         
 
       </div>
     </div>
